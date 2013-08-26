a9e06a54d644cead5cd7d3ab276ffc44f39fc5b1
Serge Cohen <serge@chocolatnoir.net>
2013-08-26 20:24:48 +0200

    On essaie dès le début d'être dans les clous de la GPL v3+.
    Serge Cohen.

---
+++ include/Anddor3Camera.h
@@ -0,0 +1,30 @@
+#ifndef ANDOR3CAMERA_H
+#define ANDOR3CAMERA_H
+
+/* andor3 plugin camera class
+ * Copyright (C) 2013 IPANEMA USR3461, CNRS/MCC.
+ * Written by Serge Cohen <serge.cohen@synchrotron-soleil.fr>
+ *
+ * This file is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this file. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
+#endif  /* ANDOR3CAMERA_H */
+
+/*
+ Local Variables:
+ c-file-style: "gnu"
+ End:
+ */
