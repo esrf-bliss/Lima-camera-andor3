5d855444264038e25061af0d7c54d10839eadf7c
Serge Cohen <serge@chocolatnoir.net>
2013-10-17 14:10:52 +0200

    Implementing Andor3CetInfoCtrlObj.

--- src/Andor3DetInfoCtrlObj.cpp
+++ src/Andor3DetInfoCtrlObj.cpp
@@ -16,6 +16,115 @@
  * License along with this file. If not, see <http://www.gnu.org/licenses/>.
  */
 
+// System headers :
+//#include <sstream>
+//#include <iostream>
+//#include <string>
+//#include <math.h>
+
+// Camera SDK headers :
+
+// LImA headers :
+
+// Andor3 plugin headers :
+#include "Andor3DetInfoCtrlObj.h"
+
+
+//---------------------------
+//- utility variables
+//---------------------------
+
+
+//---------------------------
+//- @brief constructor
+//---------------------------
+lima::Andor3::DetInfoCtrlObj::DetInfoCtrlObj(lima::Andor3::Camera& cam) :
+m_cam(cam)
+{
+  DEB_CONSTRUCTOR();
+}
+
+//---------------------------
+//- @brief destructor
+//---------------------------
+lima::Andor3::DetInfoCtrlObj::~DetInfoCtrlObj()
+{
+  DEB_DESTRUCTOR();
+}
+
+void
+lima::Andor3::DetInfoCtrlObj::getMaxImageSize(Size& max_image_size)
+{
+  DEB_MEMBER_FUNCT();
+  m_cam.getDetectorImageSize(max_image_size);
+}
+
+void
+lima::Andor3::DetInfoCtrlObj::getDetectorImageSize(Size& det_image_size)
+{
+  DEB_MEMBER_FUNCT();
+  m_cam.getDetectorImageSize(det_image_size);
+}
+
+void
+lima::Andor3::DetInfoCtrlObj::getDefImageType(ImageType& def_image_type)
+{
+  DEB_MEMBER_FUNCT();
+  m_cam.getImageType(def_image_type);
+}
+
+void
+lima::Andor3::DetInfoCtrlObj::getCurrImageType(ImageType& curr_image_type)
+{
+  DEB_MEMBER_FUNCT();
+  m_cam.getImageType(curr_image_type);
+}
+
+void
+lima::Andor3::DetInfoCtrlObj::setCurrImageType(ImageType  curr_image_type)
+{
+  DEB_MEMBER_FUNCT();
+  m_cam.setImageType(curr_image_type);
+}
+
+void
+lima::Andor3::DetInfoCtrlObj::getPixelSize(double& xsize, double& ysize)
+{
+  DEB_MEMBER_FUNCT();
+  m_cam.getPixelSize(xsize, ysize);
+}
+
+void
+lima::Andor3::DetInfoCtrlObj::getDetectorType(std::string& det_type)
+{
+  DEB_MEMBER_FUNCT();
+  m_cam.getDetectorType(det_type);
+}
+
+void
+lima::Andor3::DetInfoCtrlObj::getDetectorModel(std::string& det_model)
+{
+  DEB_MEMBER_FUNCT();
+  m_cam.getDetectorModel(det_model);
+}
+
+
+void
+lima::Andor3::DetInfoCtrlObj::registerMaxImageSizeCallback(HwMaxImageSizeCallback& cb)
+{
+  DEB_MEMBER_FUNCT();
+  // Do nothing ... So far I on't know exactly what it is suppose to do
+}
+
+void
+lima::Andor3::DetInfoCtrlObj::unregisterMaxImageSizeCallback(HwMaxImageSizeCallback& cb)
+{
+  DEB_MEMBER_FUNCT();
+  // Do nothing ... So far I on't know exactly what it is suppose to do  
+}
+
+
+
 /*
  Local Variables:
  c-file-style: "gnu"
