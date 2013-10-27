3024baefa2cae50e6549d27d726140d757ace4af
Serge Cohen <serge@chocolatnoir.net>
2013-10-27 22:40:48 +0100

    Trying to get the ROI and binning classes in.

---
+++ src/Andor3BinCtrlObj.cpp
@@ -0,0 +1,66 @@
+/* andor3 plugin binning class
+ * Copyright (C) 2013 IPANEMA USR3461, CNRS/MCC.
+ * Written by Serge Cohen <serge.cohen@synchrotron-soleil.fr>
+ *
+ * This file is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this file. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Andor3BinCtrlObj.h"
+
+//-----------------------------------------------------
+// @brief
+//-----------------------------------------------------
+lima::Andor3::BinCtrlObj::BinCtrlObj(Camera &cam) : m_cam(cam)
+{
+  DEB_CONSTRUCTOR();
+}
+
+//-----------------------------------------------------
+// @brief Dtor
+//-----------------------------------------------------
+lima::Andor3::BinCtrlObj::~BinCtrlObj()
+{
+  DEB_DESTRUCTOR();
+}
+
+//-----------------------------------------------------
+// @brief
+//-----------------------------------------------------
+void
+lima::Andor3::BinCtrlObj::getBin(Bin &aBin)
+{
+  DEB_MEMBER_FUNCT();
+  m_cam.getBin(aBin);
+}
+
+//-----------------------------------------------------
+// @brief
+//-----------------------------------------------------
+void
+lima::Andor3::BinCtrlObj::checkBin(Bin &aBin)
+{
+  DEB_MEMBER_FUNCT();
+  m_cam.checkBin(aBin);
+}
+
+//-----------------------------------------------------
+// @brief
+//-----------------------------------------------------
+void
+lima::Andor3::BinCtrlObj::setBin(const Bin& aBin)
+{
+  DEB_MEMBER_FUNCT();    
+  m_cam.setBin(aBin);
+}
+
---
+++ src/Andor3RoiCtrlObj.cpp
@@ -0,0 +1,72 @@
+/* andor3 plugin ROI class
+ * Copyright (C) 2013 IPANEMA USR3461, CNRS/MCC.
+ * Written by Serge Cohen <serge.cohen@synchrotron-soleil.fr>
+ *
+ * This file is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this file. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Andor3RoiCtrlObj.h"
+
+using namespace lima::Andor3;
+
+//-----------------------------------------------------
+// @brief Ctor
+//-----------------------------------------------------
+lima::Andor3::RoiCtrlObj::RoiCtrlObj(Camera& cam)
+: m_cam(cam)
+{
+  DEB_CONSTRUCTOR();    
+}
+
+//-----------------------------------------------------
+// @brief Dtor
+//-----------------------------------------------------
+lima::Andor3::RoiCtrlObj::~RoiCtrlObj()
+{
+  DEB_DESTRUCTOR();
+}
+
+//-----------------------------------------------------
+// @brief
+//-----------------------------------------------------
+void
+lima::Andor3::RoiCtrlObj::getRoi(Roi& roi)
+{
+  DEB_MEMBER_FUNCT();
+  m_cam.getRoi(roi);
+}
+
+//-----------------------------------------------------
+// @brief
+//-----------------------------------------------------
+void
+lima::Andor3::RoiCtrlObj::checkRoi(const Roi& set_roi, Roi& hw_roi)
+{
+  DEB_MEMBER_FUNCT();
+  m_cam.checkRoi(set_roi, hw_roi);
+}
+
+//-----------------------------------------------------
+// @brief
+//-----------------------------------------------------
+void
+lima::Andor3::RoiCtrlObj::setRoi(const Roi& roi)
+{
+  DEB_MEMBER_FUNCT();
+  Roi real_roi;
+  checkRoi(roi,real_roi);
+  m_cam.setRoi(real_roi);
+  
+}
+
